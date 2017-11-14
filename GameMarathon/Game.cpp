e0f1e8596167302632254a89310828271a6212d6
onenoorder <gerhardkroes@gmail.com>
2017-11-14 12:47:29 +0100

    Added game class

---
+++ GameMarathon/Game.cpp
@@ -0,0 +1,23 @@
+/* 
+* Game.cpp
+*
+* Created: 14-11-2017 12:44:16
+* Author: Gerhard
+*/
+
+
+#include "Game.h"
+
+// functions
+void Game::Method2()//virtual
+{
+}
+
+void Game::Method3()
+{
+}
+
+// virtual destructor
+Game::~Game()
+{
+} //~Game

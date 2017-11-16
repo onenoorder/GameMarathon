aff8a06eddae21652257335703678f2b443c4f3f
onenoorder <gerhardkroes@gmail.com>
2017-11-16 13:35:59 +0100

    Bomberman map load

--- App/App.ino
+++ App/App.ino
@@ -6,11 +6,22 @@
  */ 
 #include "Master.h"
 #include "Slave.h"
+#include "Games/Bomberman.h"
+
+Game * games[1];
 
 int main(void)
 {
+	init();
+	
 	Master master();
 	Slave slave(0);
+
+	games[0] = new Bomberman();
+	games[0]->LoadMap();
+
+	
+
 	/* Replace with your application code */
 	while (1)
 	{

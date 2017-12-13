1e6bce944d24c2731c900925eef941c198c3e139
mikenahh <harmenhilvers@hotmail.com>
2017-12-13 14:16:15 +0100

    fsfsdf

---
+++ App/Communication/IRCommunication.cpp
@@ -0,0 +1,31 @@
+/* 
+* IRCommunication.cpp
+*
+* Created: 13-12-2017 14:04:34
+* Author: Mikena
+*/
+
+
+#include "IRCommunication.h"
+
+// default constructor
+IRCommunication::IRCommunication()
+{
+} //IRCommunication
+
+
+void IRCommunication::Begin(){
+
+}
+
+void IRCommunication::Send(unsigned char data){
+
+}
+
+unsigned char IRCommunication::Receive(){
+
+}
+// default destructor
+IRCommunication::~IRCommunication()
+{
+} //~IRCommunication
